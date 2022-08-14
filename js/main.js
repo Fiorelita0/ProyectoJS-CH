7841042d9a9246343304b32ebc92122595a35408
Fiorella2003 <fiorequispe0@gmail.com>
2022-08-14 18:51:25 -0300

    se agrego storage y json

--- js/main.js
+++ js/main.js
@@ -1,67 +1,20 @@
 //nombre
+let nombreUsuario = document.getElementById('usuario');
+let nuevoNombre = prompt('Ingresá tu nombre');
+
 function solicitarNombre() {
-    let nombreUsuario = prompt('Ingresá tu nombre')
-    if (nombreUsuario == '') {
+    if (nuevoNombre == '') {
         alert("No ingresaste ningun nombre");
-    } else if (nombreUsuario == ' ') {
+    } else if (nuevoNombre == ' ') {
         alert("Ingresaste caracteres invalidos");
-    } else if (nombreUsuario == '@') {
+    } else if (nuevoNombre == '@') {
         alert("Ingresaste caracteres invalidos");
     } else {
-        alert("Bienvenido/a " + nombreUsuario);
+        console.log("Hola,  " + nuevoNombre);
     }
 }
 solicitarNombre();
-
-//edad
-function solicitarEdad() {
-    let edadUsuario = parseInt(prompt('Ingresá tu edad'))
-    let edadObligatoria = 6
-    let edadAvanzada = 40
-
-    if (edadUsuario >= edadAvanzada) {
-        console.log("Edad avanzada, juega si quiere");
-    } else if (edadUsuario < edadObligatoria) {
-        console.log("No puede jugar");
-    } else if (edadUsuario >= edadObligatoria) {
-        console.log("Puede jugar");
-    } else {
-        console.log("Juega bajo tus propios riesgos");
-    }
-}
-
-solicitarEdad();
-
-alert('Terminó el proceso')
-
-//dinero y cartas
-
-let valorCartas = 50
-
-function calculos(primerNum, valorCartas, operacion) {
-
-    switch (operacion) {
-        case '+':
-            return primerNum + valorCartas;
-        case "-":
-            return primerNum - valorCartas;
-        case "*":
-            return primerNum * valorCartas;
-        case "/":
-            return primerNum / valorCartas;
-            break;
-        default:
-            return 0;
-
-    }
-}
-console.log(calculos(50, valorCartas, '-'))
-console.log(calculos(150, valorCartas, '-'))
-console.log(calculos(100, valorCartas, '-'))
-console.log(calculos(150, valorCartas, '-'))
-console.log(calculos(200, valorCartas, '-'))
-console.log(calculos(250, valorCartas, '-'))
-console.log(calculos(300, valorCartas, '-'))
+nombreUsuario.innerText = nuevoNombre
 
 //bando
 function conBandos(mensaje1, mensaje2, mensaje3, pregunta) {
@@ -71,35 +24,34 @@ let primermensaje = conBandos('Hola!', 'Nosotros somos los pandas gigantes y est
 
 alert(primermensaje);
 
-//nombredepersonajes
-/*
-let entrada = prompt('Ingresar un apodo a tus personajes')
-while (entrada != ' ') {
-    console.log('Ingresaste como apodo' + entrada),
-        entrada = prompt('Ingresar otro apodo a tus personajes o escribe ESC para finalizar')
-}*/
 
-//rondas y din
-let maxRondas = 99
-let dinporRondas = 9999
 
-for (let i = 0; i <= maxRondas && dinporRondas; i++) {
+//desbloqueo de panda de agua
+let maxPuntaje = 49
 
-    console.log('Ronda n°' + (i + 1) + '-1');
-    console.log('Din' + ((i + 1) * 100));
+for (let i = 0; i <= maxPuntaje; i++) {
+    console.log('Puntos: ' + (i + 1) * 20);
 
-    if (i == 6) {
-        alert('No lograste 2 estrellas :|, no te rindas aun');
-    } else if (i == 3) {
-        alert('No lograste 3 estrellas :(, vamos que todavia se puede');
-    } else if (i <= 2) {
-        console.log('Sigue jugando así, y lograrás las 3 estrellas');
+    if (i == 14) {
+        alert('Lograste desbloquear el Panda de agua. Felicidades. Es un panda muy veloz');
+    } else if (i == 24) {
+        alert('Lograste desbloquear al Panda de fuego. Felicidades. Es un panda indestructible');
     } else {
-        console.log('Impresionante, jugaste con todo');
+        console.log('Sigue jugando y consigue más pandas');
     }
-
 }
-alert('Wow, ya jugaste 100 rondas')
+alert('Wow, ya lograste 1000 bambus')
+
+//array personajes
+const arrayVacio = [];
+const tiposPandas = ['Panda de tierra', 'Panda de agua', 'Panda de fuego'];
+
+console.log(tiposPandas.length)
+for (let i = 0; i < tiposPandas.length; i++) {
+    console.log(tiposPandas[i]);
+}
+
+//funcion constructora
 
 //objetos
 const usuario = {
@@ -107,19 +59,125 @@ const usuario = {
     nivel: 1,
     avatar: 'Panda de fuego'
 }
-
-//funcion constructora
-function Personajes(nombre, vida, puntos, daño) {
-    this.nombre = nombre;
-    this.vida = vida;
-    this.puntos = puntos;
-    this.daño = daño;
+class Personajes {
+    constructor(nombre, vida, puntos, daño) {
+        this.nombre = nombre.toUpperCase();
+        this.vida = vida;
+        this.puntos = puntos;
+        this.daño = daño;
+    }
+    mensaje() {
+        this.hablar = console.log('Hola soy un ' + this.nombre);
+    }
 }
-const personajepandatierra = new Personajes('Panda de tierra', 100, 50, 20);
-const personajepandaagua = new Personajes('Panda de agua', 100, 50, 50);
-const personajepandafuego = new Personajes('Panda de fuego', 100, 50, 100);
-const personajehongo = new Personajes('Hongo', 100, 50, 20);
 
-console.log(personajepandafuego)
-console.log(personajepandaagua)
-console.log(personajepandatierra)
+const personajes = [];
+personajes.push(new Personajes('Panda de tierra', 100, 50, 20));
+personajes.push(new Personajes('Panda de agua', 100, 50, 50));
+personajes.push(new Personajes('Panda de fuego', 100, 50, 100));
+personajes.push(new Personajes('Hongo', 100, 50, 20));
+
+console.log(usuario);
+console.log(personajes);
+for (const personalizados of personajes)
+    personalizados.mensaje();
+
+
+//metodos
+const puntosPersonajes = [{ id: 1, personaje: 'Panda de tierra', puntos: 50 },
+    { id: 2, personaje: 'Panda de agua', puntos: 300 },
+    { id: 3, personaje: 'Panda de fuego', puntos: 500 }
+]
+const pandadetierra = puntosPersonajes.find((personaje) => personaje.puntos === 50)
+console.log(pandadetierra);
+
+const pandadeagua = puntosPersonajes.find((personaje) => personaje.puntos === 300)
+console.log(pandadeagua);
+
+const pandadefuego = puntosPersonajes.find((personaje) => personaje.puntos === 500)
+console.log(pandadefuego);
+
+//DOM y Eventos - cambiar fondo
+
+let fondo = document.querySelector('#fondo');
+fondo.addEventListener('change', () => {
+    console.log(fondo.value);
+    document.body.style.backgroundImage = `url('img/fondode${fondo.value}.webp')`;
+});
+
+//Movimiento con mouse del Panda
+let pandaPersonaje = document.querySelector('.pandaPersonaje');
+let body = document.querySelector('body');
+
+document.addEventListener('mousemove', (event) => {
+    pandaPersonaje.style.left = (event.clientX - 80) + 'px';
+});
+
+//Generar disparo del Panda
+document.addEventListener('click', () => {
+    let balaBambu = document.createElement('div');
+    balaBambu.classList.add('balaBambu');
+    balaBambu.style.bottom = 44 + 'px';
+    balaBambu.style.left = (pandaPersonaje.getBoundingClientRect().left + 45) + 'px';
+    body.append(balaBambu);
+});
+
+//getBoundingClientRect lo saque de un tutorial para poder realizar el movimiento al panda
+
+//Movimiento del disparo
+setInterval(() => {
+    let balasBambus = document.querySelectorAll('.balaBambu');
+    balasBambus.forEach(balaBambu => {
+        balaBambu.style.top = (balaBambu.getBoundingClientRect().top - 74) + 'px'
+            //cuando topa con la pantalla se elimina
+        if (balaBambu.getBoundingClientRect().top <= -20) {
+            balaBambu.remove();
+        }
+    })
+}, 100)
+
+
+//Generar enemigos-hongos
+
+let aparicion = 0;
+
+setInterval(() => {
+    //Aparicion del hongo
+    aparicion++;
+    if (aparicion % 12 == 0) {
+        let enemigoHongo = document.createElement('div');
+        enemigoHongo.classList.add('enemigoHongo');
+        body.append(enemigoHongo);
+        enemigoHongo.style.left = (Math.random() * window.innerWidth - 100) + 'px';
+    }
+    let enemigosHongos = document.querySelectorAll('.enemigoHongo');
+    enemigosHongos.forEach(enemigoHongo => {
+        enemigoHongo.style.top = (enemigoHongo.getBoundingClientRect().top + 10) + 'px';
+        //Desaparicion del hongo
+        if (enemigoHongo.getBoundingClientRect().top >= 470) {
+            enemigoHongo.remove();
+        }
+    })
+}, 180);
+
+//Vidas del panda
+let vida = 100;
+let vida100 = document.getElementById('vida100');
+
+setInterval(() => {
+    vida--;
+    vida100.innerHTML = vida;
+
+}, 500);
+
+//Tiempo
+
+//Boton para iniciar sesion
+
+let btnInicio = document.getElementById('ingresar');
+
+btnInicio.onclick = () => {
+    location.href = "pages/ingreso.html";
+}
+
+//Modal para iniciar sesion
